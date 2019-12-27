080999a0712c2f262e59c19d6368ba475618118f
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-27 15:39:26 -0500

    both queries also now return user object

--- resolvers/projects/queries/get-projects-by-user/index.js
+++ resolvers/projects/queries/get-projects-by-user/index.js
@@ -1,11 +1,20 @@
 import Project from 'models/projects';
+import User from 'models/users/user';
 
 export const getProjectsByUser = Project => async (_, __, { user }) => {
   if (!user) {
     return [];
   }
   console.log(user.id)
-  return await Project.getByUser(user.id);
+  const QueryResults = await Project.getByUser(user.id);
+
+  //Add the full User to each result
+  const userAddedResults =  await QueryResults.map(project => {
+      return {...project, user:User.getUserById(project.user_id)}
+  });
+
+  //return query results
+  return userAddedResults
 
 
 };
--- resolvers/projects/queries/get-projects/index.js
+++ resolvers/projects/queries/get-projects/index.js
@@ -1,11 +1,19 @@
 import Project from 'models/projects';
+import User from 'models/users/user';
 
 export const getProjects = Project => async (_, __, { user }) => {
   if (!user) {
     return [];
   }
+  //Query the Database
+  const QueryResults = await Project.query();
+  //Add the full User to each result
+  const userAddedResults =  await QueryResults.map(project => {
+      return {...project, user:User.getUserById(project.user_id)}
+  });
 
-  return await Project.query();
+  //return query results
+  return userAddedResults
 
 
 };
